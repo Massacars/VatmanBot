9f068ca3cf63371a56be9d34b71dcac567f3a6a6
Massacars <massacars@gmail.com>
2018-06-06 17:24:04 +0300

    Add hashtags

--- index.js
+++ index.js
@@ -94,11 +94,26 @@ bot.onText(/\/coin/, async function(msg, match) {
 	}
 });
 
-bot.onText(/\#vatmansay/, async function(msg, match) {
+bot.onText(/\#ватфразочка/, async function(msg, match) {
 	const userId = msg.from.id;
 	const chatId = msg.chat.id;
-
-	await bot.sendMessage(userId, JSON.stringify(msg));
+	const userName = msg.from.first_name;
+	const userLastName = msg.from.last_name;
+	const username = msg.from.username;
+	const chatTitle = msg.chat.title;
+	const text = msg.text;
+	await bot.sendMessage(config.admin, userName + ' ' + userLastName + ' / ' + username + ' Чат: ' + chatTitle + ' Фраза: ' + text);
 	await bot.sendMessage(chatId, config.phrases.ty);
+});
 
+bot.onText(/\#ватідея/, async function(msg, match) {
+	const userId = msg.from.id;
+	const chatId = msg.chat.id;
+	const userName = msg.from.first_name;
+	const userLastName = msg.from.last_name;
+	const username = msg.from.username;
+	const chatTitle = msg.chat.title;
+	const text = msg.text;
+	await bot.sendMessage(config.admin, userName + ' ' + userLastName + ' / ' + username + ' Чат: ' + chatTitle + ' Ідея: ' + text);
+	await bot.sendMessage(chatId, config.phrases.ty);
 });
