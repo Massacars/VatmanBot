f0bec2f7924e3e2c0940ab91f76f7f476e34f4e7
Massacars <massacars@gmail.com>
2018-06-12 11:32:01 +0300

    Dependencies UPD / Scheduler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
 const requireFu = require('require-fu'); 
+const Scheduler = require('node-schedule');
 const bot = new TelegramBot(config.token, { polling: true });
 
 requireFu(`${__dirname}/commands`)(bot, config);
