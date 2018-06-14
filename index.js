f1ccffddcacbfb11e4e2a379c636221025e603b2
Massacars <massacars@gmail.com>
2018-06-14 14:40:00 +0300

    Connect to mongo db

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
 const requireFu = require('require-fu'); 
 const Scheduler = require('node-schedule');
+const MongoClient = require('mongodb').MongoClient;
 const bot = new TelegramBot(config.token, { polling: true });
 
-requireFu(`${__dirname}/commands`)(bot, config);
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'VatmanBot';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, async function(err, client) {
+
+	console.log("Connected successfully to server");
+
+	const db = client.db(dbName);
+
+	requireFu(`${__dirname}/commands`)(bot, config, db);
+	requireFu(`${__dirname}/schedule`)(Scheduler, bot, config, db);
+
+	process.on('SIGINT', async () => {
+		await client.close(false);
+		process.exit();
+	});	
+});
