9c9e724d53a1e2cdfade094ea8dc7757d0864d75
Massacars <massacars@gmail.com>
2018-06-14 14:41:10 +0300

    Add template based notifications

---
+++ schedule/notifications.js
@@ -0,0 +1,60 @@
+module.exports = (schedule, bot, config,  db) => {
+	const formatString = require('../util/formatString');
+	const { eatNotifString } = require('../strings/chats-strings');	
+
+	const eatFirstNotifRule = new schedule.RecurrenceRule();
+	const eatLastNotifRule = new schedule.RecurrenceRule();
+	const moneyNotifRule = new schedule.RecurrenceRule();
+	eatFirstNotifRule.hour = [9, 12, 15, 18, 21];
+	eatFirstNotifRule.minute = 46;
+	eatLastNotifRule.hour = [10, 13, 16, 19, 22];
+	eatLastNotifRule.minute = 15;
+	moneyNotifRule.hour = 19;
+	moneyNotifRule.minute = 17;
+
+	async function generateEatMessage(chatObj) {
+		let chatTag = "";
+		if(chatObj.tag){
+			chatTag = chatObj.tag;
+		}
+		return formatString(
+			eatNotifString, chatObj.name, chatTag,
+		);
+	};	
+
+	schedule.scheduleJob(eatFirstNotifRule, async () => {
+
+				
+		const notifyArr = await db.collection('chats').find({ state: true }).toArray();
+
+		notifyArr.forEach(async (chats) => {
+			const chatObj = await db.collection('chats').findOne({ _id: chats._id });
+			cheduledMsg = await bot.sendMessage(chats._id, await generateEatMessage(chatObj), {parse_mode:"HTML"});
+			bot.pinChatMessage(chats._id, cheduledMsg.message_id);
+		});
+	});
+
+	schedule.scheduleJob(eatLastNotifRule, async () => {
+
+			
+		const notifyArr = await db.collection('chats').find({ state: true }).toArray();
+
+		notifyArr.forEach(async (chats) => {
+			const chatObj = await db.collection('chats').findOne({ _id: chats._id });
+			cheduledMsg = await bot.sendMessage(chats._id, await generateEatMessage(chatObj), {parse_mode:"HTML"});
+			bot.pinChatMessage(config.chats.OM, cheduledMsg.message_id);
+		});
+	});	
+
+	schedule.scheduleJob(moneyNotifRule, async () => {
+
+		
+		const notifyArr = await db.collection('chats').find({ state: true }).toArray();
+
+		notifyArr.forEach(async (chats) => {
+			const chatObj = await db.collection('chats').findOne({ _id: chats._id });
+			cheduledMsg = await bot.sendMessage(chats._id, await generateEatMessage(chatObj), {parse_mode:"HTML"});
+			bot.pinChatMessage(chats._id, cheduledMsg.message_id);
+		});
+	});		
+}
