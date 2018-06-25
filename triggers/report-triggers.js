61d1a76286d2485c5e15f26f6d67caa46aa56ab4
Massacars <massacars@gmail.com>
2018-06-25 17:26:18 +0300

    Bugfix

--- triggers/report-triggers.js
+++ triggers/report-triggers.js
@@ -7,7 +7,7 @@ module.exports = (bot, config, db) => {
         money = msgText.match(/💵Деньги: (.*)/);
         points = msgText.match(/🏆Твой вклад: \+(.*)/);
         if (money[1] == 0 && (msg.forward_from.username === 'StartupWarsBot' || msg.forward_from.username === 'StartupWars01Bot')) {
-            await bot.sendDocument(chatId, config.files.fire, { caption: config.textmsg.money, reply_to_message_id: msgId });
+            await bot.sendDocument(chatId, config.files.fire, { caption: config.textmsg.moneyCaption, reply_to_message_id: msgId });
         };
         if (points[1] > 10000 && (msg.forward_from.username === 'StartupWarsBot' || msg.forward_from.username === 'StartupWars01Bot')) {
             await bot.sendDocument(chatId, config.files.uber, { caption: config.textmsg.vatmanCaption, reply_to_message_id: msgId });
