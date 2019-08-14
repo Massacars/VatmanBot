d027106f4536515716fa4e21a7e362b9b70d6c3a
Massacars <massacars@gmail.com>
2019-08-14 21:12:31 +0300

    Modules update

--- index.js
+++ index.js
@@ -14,26 +14,31 @@ const url = `mongodb+srv://${config.db.user}:${
 const dbName = config.db.database;
 
 // Use connect method to connect to the server
-MongoClient.connect(url, async function(err, client) {
-	if (err) {
-		console.log('MongoClient.connect err:', err.message);
-		return;
-	} else {
-		console.log('Connected successfully to server');
+MongoClient.connect(
+	url,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	async function(err, client) {
+		if (err) {
+			console.log('MongoClient.connect err:', err.message);
+			return;
+		} else {
+			console.log('Connected successfully to server');
+		}
+
+		const db = client.db(dbName);
+
+		requireFu(`${__dirname}/commands`)(bot, config, db);
+		requireFu(`${__dirname}/schedule`)(Scheduler, bot, config, db);
+		requireFu(`${__dirname}/polls`)(bot, config, db);
+		requireFu(`${__dirname}/triggers`)(bot, config, db);
+		requireFu(`${__dirname}/tops`)(bot, config, db);
+		requireFu(`${__dirname}/games`)(bot, config, db, Scheduler);
+		requireFu(`${__dirname}/tests`)(bot, config, db);
+		requireFu(`${__dirname}/teams`)(Scheduler, bot, config, db);
+
+		process.on('SIGINT', async () => {
+			await client.close(false);
+			process.exit();
+		});
 	}
-
-	const db = client.db(dbName);
-
-	requireFu(`${__dirname}/commands`)(bot, config, db);
-	requireFu(`${__dirname}/schedule`)(Scheduler, bot, config, db);
-	requireFu(`${__dirname}/polls`)(bot, config, db);
-	requireFu(`${__dirname}/triggers`)(bot, config, db);
-	requireFu(`${__dirname}/tops`)(bot, config, db);
-	requireFu(`${__dirname}/games`)(bot, config, db, Scheduler);
-	requireFu(`${__dirname}/tests`)(bot, config, db);
-
-	process.on('SIGINT', async () => {
-		await client.close(false);
-		process.exit();
-	});
-});
+);
